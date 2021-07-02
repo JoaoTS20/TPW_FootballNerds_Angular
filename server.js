018a670b10f6d622b3f92c84533cbafb9f3d39a1
JoaoTS20 <joaots@ua.pt>
2021-07-02 01:13:33 +0100

    Fix another time 2

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/angular-client'));
+app.use(express.static(__dirname + '/dist/AngularClient'));
 app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+'/dist/angular-client/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/AngularClient/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
